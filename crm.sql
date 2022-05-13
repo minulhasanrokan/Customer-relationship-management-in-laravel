931b51dc3db4e1556f7bdf97b5074e0360ddd026
Md. Minul Hasan Rokan <70903774+minulhasanrokan@users.noreply.github.com>
2022-05-13 12:20:33 +0600

    Add files via upload
    Database

---
+++ crm.sql
@@ -0,0 +1,195 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: May 13, 2022 at 08:19 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 7.4.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `crm`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `attendances`
+--
+-- Error reading structure for table crm.attendances: #1932 - Table 'crm.attendances' doesn't exist in engine
+-- Error reading data for table crm.attendances: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `crm`.`attendances`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customers`
+--
+-- Error reading structure for table crm.customers: #1932 - Table 'crm.customers' doesn't exist in engine
+-- Error reading data for table crm.customers: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `crm`.`customers`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `departments`
+--
+-- Error reading structure for table crm.departments: #1932 - Table 'crm.departments' doesn't exist in engine
+-- Error reading data for table crm.departments: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `crm`.`departments`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `designations`
+--
+-- Error reading structure for table crm.designations: #1932 - Table 'crm.designations' doesn't exist in engine
+-- Error reading data for table crm.designations: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `crm`.`designations`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `districts`
+--
+-- Error reading structure for table crm.districts: #1932 - Table 'crm.districts' doesn't exist in engine
+-- Error reading data for table crm.districts: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `crm`.`districts`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `divisions`
+--
+-- Error reading structure for table crm.divisions: #1932 - Table 'crm.divisions' doesn't exist in engine
+-- Error reading data for table crm.divisions: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `crm`.`divisions`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `failed_jobs`
+--
+-- Error reading structure for table crm.failed_jobs: #1932 - Table 'crm.failed_jobs' doesn't exist in engine
+-- Error reading data for table crm.failed_jobs: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `crm`.`failed_jobs`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `freelancers`
+--
+-- Error reading structure for table crm.freelancers: #1932 - Table 'crm.freelancers' doesn't exist in engine
+-- Error reading data for table crm.freelancers: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `crm`.`freelancers`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `holydays`
+--
+-- Error reading structure for table crm.holydays: #1932 - Table 'crm.holydays' doesn't exist in engine
+-- Error reading data for table crm.holydays: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `crm`.`holydays`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `holyday_types`
+--
+-- Error reading structure for table crm.holyday_types: #1932 - Table 'crm.holyday_types' doesn't exist in engine
+-- Error reading data for table crm.holyday_types: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `crm`.`holyday_types`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `leave_applications`
+--
+-- Error reading structure for table crm.leave_applications: #1932 - Table 'crm.leave_applications' doesn't exist in engine
+-- Error reading data for table crm.leave_applications: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `crm`.`leave_applications`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+-- Error reading structure for table crm.migrations: #1932 - Table 'crm.migrations' doesn't exist in engine
+-- Error reading data for table crm.migrations: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `crm`.`migrations`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `notices`
+--
+-- Error reading structure for table crm.notices: #1932 - Table 'crm.notices' doesn't exist in engine
+-- Error reading data for table crm.notices: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `crm`.`notices`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `password_resets`
+--
+-- Error reading structure for table crm.password_resets: #1932 - Table 'crm.password_resets' doesn't exist in engine
+-- Error reading data for table crm.password_resets: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `crm`.`password_resets`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `personal_access_tokens`
+--
+-- Error reading structure for table crm.personal_access_tokens: #1932 - Table 'crm.personal_access_tokens' doesn't exist in engine
+-- Error reading data for table crm.personal_access_tokens: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `crm`.`personal_access_tokens`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `projects`
+--
+-- Error reading structure for table crm.projects: #1932 - Table 'crm.projects' doesn't exist in engine
+-- Error reading data for table crm.projects: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `crm`.`projects`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `project_types`
+--
+-- Error reading structure for table crm.project_types: #1932 - Table 'crm.project_types' doesn't exist in engine
+-- Error reading data for table crm.project_types: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `crm`.`project_types`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sessions`
+--
+-- Error reading structure for table crm.sessions: #1932 - Table 'crm.sessions' doesn't exist in engine
+-- Error reading data for table crm.sessions: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `crm`.`sessions`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `unions`
+--
+-- Error reading structure for table crm.unions: #1932 - Table 'crm.unions' doesn't exist in engine
+-- Error reading data for table crm.unions: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `crm`.`unions`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `upazilas`
+--
+-- Error reading structure for table crm.upazilas: #1932 - Table 'crm.upazilas' doesn't exist in engine
+-- Error reading data for table crm.upazilas: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `crm`.`upazilas`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+-- Error reading structure for table crm.users: #1932 - Table 'crm.users' doesn't exist in engine
+-- Error reading data for table crm.users: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `crm`.`users`' at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
